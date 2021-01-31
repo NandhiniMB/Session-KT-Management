7ddf10040caf7b9bea9c4ae238286238b5b22f30
NandhiniMB-Accolite <nandhini.mb@accolitedigital.com>
2021-01-31 22:33:04 +0530

    Added Repos

--- internalPortal/src/main/java/com/team6/internetPortal/repository/impl/VideoRepositoryImpl.java
+++ internalPortal/src/main/java/com/team6/internetPortal/repository/impl/VideoRepositoryImpl.java
@@ -1,9 +1,12 @@
 package com.team6.internetPortal.repository.impl;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.team6.internetPortal.entity.User;
@@ -14,5 +17,5 @@ import com.team6.internetPortal.repository.IVideoRepository;
 @Transactional
 public abstract class VideoRepositoryImpl implements IVideoRepository {
 	
-
+	
 }

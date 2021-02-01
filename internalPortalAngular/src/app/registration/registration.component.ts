ad8e0f366287b2bdc849bc799dfcee08ddf4dbd7
Gaurang-Athavale <gaurang.athavale@accolitedigital.com>
2021-02-01 16:02:56 +0530

    Added frontend for login

---
+++ internalPortalAngular/src/app/registration/registration.component.ts
@@ -0,0 +1,63 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from '../Models/User';
+import { RegistrationService } from '../registration.service';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.scss']
+})
+export class RegistrationComponent implements OnInit {
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+  passwordFormControl = new FormControl(null, [
+    Validators.required,
+    // Validators.email,
+  ]);
+
+  usernameFormControl = new FormControl(null, [
+    Validators.required,
+    // Validators.email,
+  ]);
+
+  nameFormControl = new FormControl(null, [
+    Validators.required,
+    // Validators.email,
+  ]);
+
+  user: User = new User();
+
+  message: String  = '';
+
+
+  constructor(private service: RegistrationService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  registerUser() {
+    this.service.registerUserFromRemote(this.user).subscribe(
+      data => { 
+        console.log("Response Received");
+        this.router.navigate(['/login']);
+      },
+      error => {
+        this.message = "The Email Id already Exists!!";
+        console.log("Exception Occurred");
+      } 
+    );
+  }
+
+  loginUser() {
+    this.router.navigate(['/login']);
+    console.log("navigated to login page");
+  }
+
+}

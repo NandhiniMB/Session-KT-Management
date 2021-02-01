e471dc9c5612ed76d15187261664aa6906061e2a
NandhiniMB-Accolite <nandhini.mb@accolitedigital.com>
2021-02-01 18:58:01 +0530

    Added Constants

---
+++ internalPortal/src/main/java/com/team6/internetPortal/constants/Constants.java
@@ -0,0 +1,10 @@
+package com.team6.internetPortal.constants;
+
+public final class Constants {
+
+    private Constants() {
+            // restrict instantiation
+    }
+
+    public enum status{APPROVED,PENDING,REJECTED};
+}

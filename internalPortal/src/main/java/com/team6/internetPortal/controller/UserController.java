102005775ea257f34dc371d5a0826a57eba20323
NandhiniMB-Accolite <nandhini.mb@accolitedigital.com>
2021-02-01 13:39:38 +0530

    modified controller

--- internalPortal/src/main/java/com/team6/internetPortal/controller/UserController.java
+++ internalPortal/src/main/java/com/team6/internetPortal/controller/UserController.java
@@ -15,52 +15,52 @@ public class UserController {
 	@Autowired
     private IUserService userService;
 
-//    "/video"
+	    @PostMapping("/register")
+	    @CrossOrigin(origins = "http://localhost:4200")
+	    public User registerUser(@RequestBody User user) throws Exception{
+	        String tempEmail = user.getEmail();
 
-    @PostMapping("/register")
-    public User registerUser(@RequestBody User user) throws Exception{
-        String tempEmail = user.getEmail();
+	        // Check if null or empty
+	        if(tempEmail != null && !"".equals(tempEmail)){
 
-        // Check if null or empty
-        if(tempEmail != null && !"".equals(tempEmail)){
+	            // Check if no user found with entered emailId
+	            if(userService.fetchUserByEmailId(tempEmail) != null){
+	                throw new Exception("User with "+ tempEmail +" already exists!");
+	            }
+	        }
+	        user.setCreatedOn(new Date(System.currentTimeMillis()));
 
-            // Check if no user found with entered emailId
-            if(userService.fetchUserByEmailId(tempEmail) != null){
-                throw new Exception("User with "+ tempEmail +" already exists!");
-            }
-        }
-        user.setCreatedOn(new Date(System.currentTimeMillis()));
+	        User userObj;
+	        userObj = userService.saveUser(user);
+	        return userObj;
+	    }
 
-        User userObj;
-        userObj = userService.saveUser(user);
-        return userObj;
-    }
+	    @PostMapping("/login")
+	    @CrossOrigin(origins = "http://localhost:4200")
+	    public User loginUser(@RequestBody User user) throws Exception{
+	        String tempEmail = user.getEmail();
+	        String tempPassword = user.getPassword();
+	        User userCheck = null;
 
-    @PostMapping("/login")
-    public User loginUser(@RequestBody User user) throws Exception{
-        String tempEmail = user.getEmail();
-        String tempPassword = user.getPassword();
-        User userCheck = null;
+	        // Check if null or empty
+	        if(tempEmail != null && !"".equals(tempEmail)){
+	            userCheck = userService.fetchUserByEmailId(tempEmail);
 
-        // Check if null or empty
-        if(tempEmail != null && !"".equals(tempEmail)){
-            userCheck =userService.fetchUserByEmailId(tempEmail);
+	            // Check if no user found with entered emailId
+	            if(userCheck != null){
+//	                System.out.println(userCheck.getEmail()+" "+ userCheck.getPassword()+ " "+ tempPassword);
 
-            // Check if no user found with entered emailId
-            if(userCheck != null){
-                System.out.println(userCheck.getEmail()+" "+ userCheck.getPassword()+ " "+ tempPassword);
-
-                // Check Email-Password Match
-                if(!(userCheck.getPassword()).equals(tempPassword)){
-                    throw new Exception("Username and Passwords do not match");
-                }
-            }
-            else{
-                throw new Exception("Your Email Id is not registered :(");
-            }
-        }
-        return userCheck;
-    }
+	                // Check Email-Password Match
+	                if(!(userCheck.getPassword()).equals(tempPassword)){
+	                    throw new Exception("Username and Passwords do not match");
+	                }
+	            }
+	            else{
+	                throw new Exception("Your Email Id is not registered :(");
+	            }
+	        }
+	        return userCheck;
+	    }
 
 	
 

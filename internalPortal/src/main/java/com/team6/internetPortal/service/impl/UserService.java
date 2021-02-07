4ac18716f0e6b6bd6869f97b95880665b660e375
Gaurang-Athavale <gaurang.athavale@accolitedigital.com>
2021-02-07 11:48:56 +0530

    Forgot Password and Toolbar fixed

--- internalPortal/src/main/java/com/team6/internetPortal/service/impl/UserService.java
+++ internalPortal/src/main/java/com/team6/internetPortal/service/impl/UserService.java
@@ -15,11 +15,25 @@ public class UserService implements IUserService{
 	@Autowired
     private IUserRepository userRepository;
 
+	@Autowired
+	private SendMailService  sendmailService;
+
 	public User saveUser(User user){
 	        return userRepository.save(user);
 	    }
 
-	    public User fetchUserByEmailId(String email){
+	public User fetchUserByEmailId(String email){
 	        return userRepository.findByEmail(email);
 	    }
+
+	public User forgotPassword(String email){
+		User user = userRepository.findByEmail(email);
+		user.setPassword("Accolite");
+		System.out.println(user.getPassword());
+		String content = "Your new password is: \"Accolite\"";
+		String userEmails[] = new String[1];
+		userEmails[0] = email;
+		sendmailService.sendEmail(userEmails, content);
+		return userRepository.save(user);
+	}
 }
